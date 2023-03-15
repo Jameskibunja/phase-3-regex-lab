4e35ed25bba9859a38d141595eff315f7c33f0ed
JAMES <jameskibunja17@gmail.com>
2023-03-15 15:23:08 +0300

    Done Work

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,20 @@
 def starts_with_a_vowel?(word)
-
-end
-
-def words_starting_with_un_and_ending_with_ing(text)
-
-end
-
-def words_five_letters_long(text)
-
-end
-
-def first_word_capitalized_and_ends_with_punctuation?(text)
-
-end
-
-def valid_phone_number?(phone)
-
-end
+    word.downcase.match?(/\A[aeiou]/)
+  end
+  
+  def words_starting_with_un_and_ending_with_ing(text)
+    text.downcase.scan(/\bun\w*ing\b/)
+  end
+  
+  def words_five_letters_long(text)
+    text.downcase.scan(/\b\w{5}\b/)
+  end
+  
+  def first_word_capitalized_and_ends_with_punctuation?(text)
+    text.match?(/^[A-Z].*[[:punct:]]\z/)
+  end
+  
+  def valid_phone_number?(phone)
+    phone.match?(/\A\(*\d{3}\)*[\s.-]?\d{3}[\s.-]?\d{4}\z/)
+  end
+  
